ed72debe6ca22b241ca7c431982fccb7a0da5b08
Krisnandika <krisnandikaaji@gmail.com>
2019-11-05 16:54:55 +0700

    incom

---
+++ AnjayFramework/Anjay.swift
@@ -0,0 +1,17 @@
+//
+//  Anjay.swift
+//  AnjayFramework
+//
+//  Created by Krisnandika Aji on 05/11/19.
+//  Copyright © 2019 Krisnandika Aji. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class Anjay: NSObject {
+    
+    public func mabar() {
+        NSLog("You have anjayed your last mabar.")
+    }
+}
---
+++ AnjayFramework/AnjayFramework.h
@@ -0,0 +1,19 @@
+//
+//  AnjayFramework.h
+//  AnjayFramework
+//
+//  Created by Krisnandika Aji on 05/11/19.
+//  Copyright © 2019 Krisnandika Aji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnjayFramework.
+FOUNDATION_EXPORT double AnjayFrameworkVersionNumber;
+
+//! Project version string for AnjayFramework.
+FOUNDATION_EXPORT const unsigned char AnjayFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnjayFramework/PublicHeader.h>
+
+
